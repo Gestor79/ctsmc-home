5fee805b2867a2c989196e63c99957ca200f6087
Gestor79 <gestor.planilhas.ctsmc@gmail.com>
2025-06-26 11:54:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,14 +94,14 @@ links.onclick = function clear() {
                 <p class="m-title">PONTO <br>ELETRONICO</p>
             </div>
             
-    <div class="bt">
-        <a class="link-bt" href="https://mail.sms.maceio.al.gov.br/#1" target="_blank">
-            <div>
-                <img class="logo-op" src="./img/ZIMBRA.png" alt="logo ZIMBRA">
-            </div>
-        </a>
-        <p class="m-title">ZIMBRA</p>
-    </div>
+    // <div class="bt">
+    //     <a class="link-bt" href="https://mail.sms.maceio.al.gov.br/#1" target="_blank">
+    //         <div>
+    //             <img class="logo-op" src="./img/ZIMBRA.png" alt="logo ZIMBRA">
+    //         </div>
+    //     </a>
+    //     <p class="m-title">ZIMBRA</p>
+    // </div>
 
 
     <div class="bt">
